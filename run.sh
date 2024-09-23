1ca8ebc320ee86ed754d66aa05a1b90c7002d70b
Lena Rozalska <therealeagle.yt@gmail.com>
2024-09-23 10:07:42 +0200

    updated run script for rpi

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # Path to the askpass script
-export SUDO_ASKPASS=$(pwd)/askpass.sh
+if [ "$(id -u)" != "0" ]; then
+     echo "This script must be run as root" 1>&2
+     export SUDO_ASKPASS=~/WebstormProjects/HomePilotJSServer/askpass.sh
+fi
+
+# Set the DISPLAY environment variable
+export DISPLAY=:0
 
 # Detect the available serial port
 PORT=$(ls /dev/ttyACM* | head -n 1)
+
+# If no port is found on ACM, try AMA
+if [ -z "$PORT" ]; then
+  PORT=$(ls /dev/ttyAMA* | head -n 1)
+fi
 
 if [ -z "$PORT" ]; then
   echo "No serial port found."
@@ -16,6 +27,3 @@ sudo -A chmod 666 "$PORT"
 
 # Export the port so it's available to the TypeScript script
 export MY_VAR="$PORT"
-
-# Run the TypeScript script using the locally installed ts-node
-npx ts-node --transpile-only src/main.ts
